8e4e83d1477b1a81f22bf4c1fd0caa26694130a7
Deviationlark <zilamovpetar@gmail.com>
2023-07-07 00:31:34 +0200

    Add project files.

---
+++ Math Game/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MathGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+}
+
+    
+
+
