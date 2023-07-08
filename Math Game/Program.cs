dcae0f5411bd4f5a17b06841c739d3ab840fea5d
Deviationlark <zilamovpetar@gmail.com>
2023-07-08 23:50:31 +0200

    Added menu's

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -4,10 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-        }
-    }
+            string option;
+            Console.WriteLine("MATH GAME");
+            Console.WriteLine("1. Play");
+            Console.WriteLine("2. History");
+            Console.WriteLine("3. Exit");
+            Console.Write("Choose: ");
+            option = Console.ReadLine();
 
+            Console.ReadLine();
+
+        }   
+        
+    }
 }
 
     
